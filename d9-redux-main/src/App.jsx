a7c30df4d9619f77591667317422e73a0f335ffa
Inchino <alessandro.incalza22@gmail.com>
2025-01-20 17:11:28 +0100

    Meglio

--- d9-redux-main/src/App.jsx
+++ d9-redux-main/src/App.jsx
@@ -1,22 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import MainSearch from './components/MainSearch'
-import CompanySearchResults from './components/CompanySearchResults'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Favourites from './components/Favourites'
-import ListIndicator from './components/Favourites'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MainSearch from './components/MainSearch';
+import CompanySearchResults from './components/CompanySearchResults';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Favourites from './components/Favourites';
+import ListIndicator from './components/ListIndicator';
 
 function App() {
   return (
     <BrowserRouter>
-    <ListIndicator/>
+      <ListIndicator />
       <Routes>
         <Route path="/" element={<MainSearch />} />
         <Route path="/:company" element={<CompanySearchResults />} />
         <Route path="/favourites" element={<Favourites />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- d9-redux-main/src/components/ListIndicator.jsx
+++ d9-redux-main/src/components/ListIndicator.jsx
@@ -1,24 +1,24 @@
-import { Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ListIndicator = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const content = useSelector((state) => {
-    return state.list.content
-  })
+    return state.list.content;
+  });
 
   return (
-    <div className="d-flex justify-content-end my-4">
+    <div className="d-flex justify-content-center my-4">
       <Button
-        onClick={() => navigate('/favourites')}
+        onClick={() => navigate("/favourites")}
         className="d-flex align-items-center"
       >
-        <h3>Lista preferiti</h3>
+        <h3>Preferiti: </h3>
         <span className="ms-2">{content.length}</span>
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default ListIndicator
+export default ListIndicator;
