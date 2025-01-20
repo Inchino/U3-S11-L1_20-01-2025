cef0105b7e7ed19febcf6f77487306d35b9cecb6
Inchino <alessandro.incalza22@gmail.com>
2025-01-20 17:26:44 +0100

    Non funzionante

--- d9-redux-main/src/components/Favourites.jsx
+++ d9-redux-main/src/components/Favourites.jsx
@@ -1,35 +1,36 @@
-import { Col, Row, Button } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
+import { Col, Row, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Job from "./Job";
 
-const List = () => {
-  const list = useSelector((state) => {
-    return state.list.content
-  })
-
-  const dispatch = useDispatch()
+const Favourites = () => {
+  const list = useSelector((state) => state.list.content); // Ottieni la lista dei preferiti
+  const dispatch = useDispatch();
 
   return (
     <Row>
       <Col sm={12}>
-        <ul style={{ listStyle: 'none' }}>
-          {list.map((job, i) => (
-            <li key={i} className="my-4">
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          {list.map((job) => (
+            <li key={job._id} className="my-4 d-flex align-items-center">
               <Button
                 variant="danger"
+                className="me-3"
                 onClick={() => {
                   dispatch({
-                    type: 'REMOVE_FROM_CART',
-                    payload: i,
-                  })
+                    type: "REMOVE_FROM_CART",
+                    payload: job._id,
+                  });
                 }}
               >
-              </Button> 
+                Rimuovi
+              </Button>
+              <Job data={job} />
             </li>
           ))}
         </ul>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default List;
+export default Favourites;
